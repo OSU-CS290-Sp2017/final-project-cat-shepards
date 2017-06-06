0be9e07b9642ce764a73e486e89f5fd8c56360c5
Garen Porter <porterga@flip1.engr.oregonstate.edu>
2017-06-05 17:32:02 -0700

    Basic server

---
+++ server.js
@@ -0,0 +1,44 @@
+var path = require('path');
+var fs = require('fs');
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var twit_data = require('./catData');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res, next){
+
+  var templateArgs = {
+    author: catData;
+    caption: catData;
+    url: catData;
+    votes: catData;
+  };
+  res.render('catPage', templateArgs);
+});
+
+app.get('/pawpular', function(req, res, next){
+
+  var templateArgs = {
+    author: catData;
+    caption: catData;
+    url: catData;
+    votes: catData;
+  };
+  res.render('catPage', templateArgs);
+
+});
+
+app.get('*', function(req, res, next){
+
+  res.status(404).render('404Page');
+
+});
+
+app.listen(port, function() {
+  console.log("Server listening on port", port);
+});
