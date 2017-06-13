0ca9495b9524df5fd272872c61818df9296a8222
Chase <deneckec@oregonstate.edu>
2017-06-13 13:39:35 -0700

    Chase's working code for addCat

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ var catData = require('./catData');
 var app = express();
 var port = process.env.PORT || 3000;
 
-var mongoHost = process.env.MONGO_HOST || "classmongo.engr.oregonstate.edu";
-var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoHost = process.env.MONGO_HOST || "ds143151.mlab.com";
+var mongoPort = process.env.MONGO_PORT || 43151;
 var mongoUser = process.env.MONGO_USER || "cs290_janzeng";
-var mongoPassword = process.env.MONGO_PASSWORD || "youshallnotpass";
-var mongoDBName = process.env.MONGO_DB || "cs290_janzeng";
+var mongoPassword = process.env.MONGO_PASSWORD || "y0ushallnotpass";
+var mongoDBName = process.env.MONGO_DB || "heroku_bp42bgrj";
 var mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword +
   '@' + mongoHost + ':' + mongoPort + '/' + mongoDBName;
 var mongoDB;
@@ -111,10 +111,11 @@ app.post('/upvote',function(req, res, next) {
   });
 });
 
-app.post('/newCat', function(req, res, next) {
+app.post("/newCat", function(req, res, next) {
   var collection = mongoDB.collection('cats');
-  console.log()
-  collection.insert(postBody);
+  collection.insert(req.body);
+  res.status(200).send();
+  
 });
 
 app.get('*', function(req, res, next){
